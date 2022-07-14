a1a4db653dad34ca136b1c7be21df63c683afaea
Matthewechong <78461390+Matthewechong@users.noreply.github.com>
2022-07-13 20:26:00 -0700

    Add files via upload

---
+++ tic_tac_toe_backend_spring/src/main/java/com/tic_tac_web_server/controller/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.tic_tac_web_server.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
