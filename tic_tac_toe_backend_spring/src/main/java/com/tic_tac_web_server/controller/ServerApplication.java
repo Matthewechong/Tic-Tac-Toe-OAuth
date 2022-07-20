502a98ae25c48eb34698f43c93daaddfd57d2aad
mchong <mchong@aseg.com>
2022-07-20 12:38:06 -0700

    cors-issues

--- tic_tac_toe_backend_spring/src/main/java/com/tic_tac_web_server/controller/ServerApplication.java
+++ tic_tac_toe_backend_spring/src/main/java/com/tic_tac_web_server/controller/ServerApplication.java
@@ -2,8 +2,10 @@ package com.tic_tac_web_server.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class ServerApplication {
 
 	public static void main(String[] args) {
